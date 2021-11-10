cd877c1b5e5c08921676d9283a36dbf9d884d9da
jody <89944323+JodyChow@users.noreply.github.com>
2021-11-09 20:13:18 -0500

    finished website ?

---
+++ week03-2/main.js
@@ -0,0 +1,10 @@
+$(".post1").draggable()
+$(".post2").draggable()
+$(".post3").draggable()
+$(".post4").draggable()
+$(".post5").draggable()
+$(".post6").draggable()
+$(".post7").draggable()
+
+$(".bgcred1").draggable()
+$(".videolink").draggable()
